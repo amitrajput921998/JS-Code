ab1088391d5866655034305ffef0a5f6bd1c1974
Amit Singh <49475880+amitrajput921998@users.noreply.github.com>
2023-11-12 20:41:45 +0530

    Add files via upload

---
+++ variable.js
@@ -0,0 +1,15 @@
+console.log("hello");
+const accountId=1234
+let accountEmail="se@gmail.com"
+var accountPassword="1234"
+accountcity="Agar"
+
+// accountId=2
+
+accountEmail="e@gmail.com"
+accountPassword="12234567"
+accountcity="japit"
+
+console.log(accountId);
+
+console.table([accountEmail,accountPassword,accountId])
