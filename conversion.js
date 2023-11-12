0ea0d8396075079636f7b05708f75412981656ca
Amit Singh <49475880+amitrajput921998@users.noreply.github.com>
2023-11-12 21:00:43 +0530

    Add files via upload

---
+++ conversion.js
@@ -0,0 +1,25 @@
+// let score=undefined
+
+// console.log(typeof score);
+
+// let valueNumber=Number(score);
+
+// console.log(typeof valueNumber);
+
+// console.log(valueNumber)
+
+// "33"=>33
+// "33abc"=>NaN
+// true=> 1 false=>0
+
+let islogg=1
+
+let boollog=Boolean(islogg)
+
+console.log(boollog)
+
+// 1=> true
+// 0 => false
+
+// "amit"=> true
+
