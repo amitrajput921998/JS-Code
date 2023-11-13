98f798085fdeccc5fffe1f67eef5781e36fc6762
Amit Singh <49475880+amitrajput921998@users.noreply.github.com>
2023-11-13 10:09:44 +0530

    Add files via upload

---
+++ operation.js
@@ -0,0 +1,43 @@
+// // let value=3
+
+// // let negValue=-value
+
+// // console.log(negValue)
+
+// // console.log(5%2);
+
+// // let str1="hello"
+// // let str2="hi"
+
+// // console.log(str1+str2);
+
+// // console.log("1"+2);
+
+// // console.log(1+"2");
+
+// console.log("1"+2+2);  //here first string is there so string to number will get high priorty
+
+// console.log(1+2+"2"); //there first number is there so it will do number conversion first then it add the string
+
+// // console.log(3+4*5%3);
+
+// // console.log((3+4)/10);
+
+// // console.log(+true);
+
+// let num1,num2,num3
+
+// num1=num2=num3=2+3
+
+// console.table([num1,num2,num3])
+
+// let g=100
+// g++
+// console.log(g);
+
+let x=3;
+const y=x++
+
+console.log(`x:${x},y:${y}`)
+
+
