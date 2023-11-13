46de852386e7146429f56c8853f4a917b9a437c2
Amit Singh <49475880+amitrajput921998@users.noreply.github.com>
2023-11-13 12:18:51 +0530

    Add files via upload

---
+++ nummath.js
@@ -0,0 +1,38 @@
+const score=100;
+
+console.log(score);
+
+const balance=new Number(200)
+
+console.log(balance.toString().length);
+console.log(balance.toFixed(2));
+
+const otherNumber=23.8966
+
+console.log(otherNumber.toPrecision(3));
+
+const hundreds=100000
+
+console.log(hundreds.toLocaleString('en-IN'))
+
+
+Math
+
+console.log(Math);
+
+console.log(Math.abs(-34));
+
+console.log(Math.round(4.3));
+
+console.log(Math.ceil(4.2));
+
+console.log(Math.floor(4.2));
+
+console.log(Math.max(3,4,5,6));
+
+console.log((Math.random()*10)+1);
+
+const min=10
+const max=20
+
+console.log(Math.floor(Math.random()*(max-min+1))+min);
