59507ff38a2ec4e8109c3771dd65931499e8bd08
Amit Singh <49475880+amitrajput921998@users.noreply.github.com>
2023-11-15 23:33:49 +0530

    Add files via upload

---
+++ whileloop.js
@@ -0,0 +1,23 @@
+
+let i=0
+while (i<=5){
+    console.log(`value of index is ${i}`);
+    i=i+2
+}
+
+let myarray=['amit','singh','kumar']
+
+let arr=0
+while(arr<myarray.length){
+    console.log(`Value is ${myarray[arr]}`);
+    arr=arr+1
+}
+
+// do while loop
+
+let score=11
+
+do{
+    console.log(`Score is ${score}`);
+    score++
+}while(score<=10)
